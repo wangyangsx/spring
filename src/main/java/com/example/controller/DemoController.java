cfa10bb6e089febae9f2df6483e81e1aeedd6e57
Administrator <https://github.com/wangyangsx/GitTest.git>
2019-08-28 14:42:19 +0800

    开始

---
+++ src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RequestMethod;
+        import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @ResponseBody
+    public String  say(){
+        return "hello";
+    }
+}
