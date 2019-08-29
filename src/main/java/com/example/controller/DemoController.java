c1728fdfef447e5a7b023276332fe6d50010bdd2
Administrator <https://github.com/wangyangsx/GitTest.git>
2019-08-29 09:11:17 +0800

    添加MyabtisPlus

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -1,15 +1,31 @@
 package com.example.controller;
 
+        import com.example.pojo.User;
+        import com.example.service.UserService;
+        import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.stereotype.Controller;
         import org.springframework.web.bind.annotation.RequestMapping;
         import org.springframework.web.bind.annotation.RequestMethod;
         import org.springframework.web.bind.annotation.ResponseBody;
 
+        import java.util.List;
+
 @Controller
 public class DemoController {
+
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     @ResponseBody
     public String  say(){
         return "hello";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/findall")
+    public List<User> findAll(){
+        List<User> userList = userService.findAll();
+        return userList;
     }
 }
